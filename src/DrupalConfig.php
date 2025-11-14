b6bf8094bbcf5b0ffb98551c6b02668a29156d68
Dave Reid <dave@davereid.net>
2025-11-14 12:59:53 -0600

    Use drupal-finder to support non-web install directories.

--- src/DrupalConfig.php
+++ src/DrupalConfig.php
@@ -7,6 +7,7 @@ namespace TwigCsFixerDrupal;
 use Drupal\Core\Extension\ModuleHandlerInterface;
 use Drupal\Core\Template\TwigTransTokenParser;
 use Drupal\Core\Theme\ThemeManagerInterface;
+use DrupalFinder\DrupalFinderComposerRuntime;
 use TwigCsFixer\Config\Config;
 use TwigCsFixer\Rules\Whitespace\IndentRule;
 use TwigCsFixer\Standard\TwigCsFixer;
@@ -39,9 +40,9 @@ class DrupalConfig {
 
     // Add drupal/storybook support.
     if (class_exists('\TwigStorybook\Twig\TwigExtension') && class_exists('\TwigStorybook\Service\StoryCollector')) {
-      $drupalRoot = 'web';
+      $finder = new DrupalFinderComposerRuntime();
       $storyCollector = new \TwigStorybook\Service\StoryCollector();
-      $config->addTwigExtension(new \TwigStorybook\Twig\TwigExtension($storyCollector, $drupalRoot));
+      $config->addTwigExtension(new \TwigStorybook\Twig\TwigExtension($storyCollector, $finder->getDrupalRoot()));
     }
 
     // Load default ruleset.
